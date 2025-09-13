afaa33115d4a80c5aa6759022164018670ef6f82
yash3525 <martingrx3525@gmail.com>
2025-09-13 15:47:28 +0530

    Added new Route

--- web-service/app.py
+++ web-service/app.py
@@ -3,16 +3,21 @@ import os
 from flask import Flask
 
 app=Flask(__name__)
-ANALYTICS_URl=os.environ.get("ANALYTICS_URL","http://analytics:5001/visit")
+ANALYTICS_URL=os.environ.get("ANALYTICS_URL","http://analytics:5001/visit")
 @app.route('/')
 def home():
-
 	requests.get(ANALYTICS_URL)
 	return "Hello From Web Service!"
 
 @app.route('/about')
 def about():
 	return "Web Service in Microservice Demo"
+
+@app.route('/newRoute')
+def newRoute():
+	return "This is a new Route"
+
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
 
